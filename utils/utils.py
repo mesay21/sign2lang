73c462edad8c12c745fa80177a924eef87ec06c5
mesaybejiga <mesay.bejiga@gmail.com>
2020-06-14 23:25:07 +0200

    feat: Add image normalizer function

--- utils/utils.py
+++ utils/utils.py
@@ -29,3 +29,23 @@ def decode_image(encoded_image):
     img = tf.io.decode_jpeg(encoded_image)
     img.set_shape((256, 256, 3))
     return decode_image
+
+
+def normalize(image):
+    '''Normalize input image by subtracting mean and standard deviation. The mean
+    and standard deviation values are taken from ImageNet data.
+
+    Args:
+        image--> Image tensor of shape 3D (H x W x C) 
+    Returns:
+        image--> Normalized image tensor
+    '''
+
+    imagenet_mean = tf.reshape(tf.constant([0.485, 0.456, 0.406]), (1, 1, -1))
+    imagenet_std = tf.reshape(tf.constant([0.229, 0.224, 0.225]), (1, 1, -1))
+
+    image = tf.cast(image, tf.float32) / 255.
+
+    image = (image - imagenet_mean) / imagenet_std
+
+    return image
